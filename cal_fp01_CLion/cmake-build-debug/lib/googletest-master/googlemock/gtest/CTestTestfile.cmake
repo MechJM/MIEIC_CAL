975177090d7e54e0f89899e5688966fac78982bf
MechJM <matosjoaops@protonmail.com>
2020-02-20 21:09:59 +0000

    Fixed permission issues and added .gitignore

---
+++ cal_fp01_CLion/cmake-build-debug/lib/googletest-master/googlemock/gtest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/mechjm/Repositórios - Faculdade/2.º ano/2.º semestre/CAL/cal_fp01_CLion/lib/googletest-master/googletest
+# Build directory: /home/mechjm/Repositórios - Faculdade/2.º ano/2.º semestre/CAL/cal_fp01_CLion/cmake-build-debug/lib/googletest-master/googlemock/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
