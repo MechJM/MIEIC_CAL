c83946ea488852814d2a7d003c9d77d4f6207ca9
MechJM <matosjoaops@protonmail.com>
2020-02-28 22:05:34 +0000

    Reorganized repo

---
+++ README.md
@@ -0,0 +1,1 @@
+# MIEIC - CAL
--- cal_fp01_CLion/Tests/Sum.cpp
+++ cal_fp01_CLion/Tests/Sum.cpp
@@ -5,8 +5,27 @@
 #include "Sum.h"
 #include <chrono>
 
+using namespace std;
+
 string calcSum(int* sequence, int size)
 {
-	return "";
-}
+    string result;
 
+
+
+    for (int i=1;i<=size;i++)
+    {
+        int currentMin = 9999;
+        int currentSum = 0;
+
+        for (int i2=0;i2<i;i2++)
+        {
+            currentSum+=sequence[i2];
+        }
+        if (currentSum < currentMin)
+
+    }
+
+
+    return result;
+}
