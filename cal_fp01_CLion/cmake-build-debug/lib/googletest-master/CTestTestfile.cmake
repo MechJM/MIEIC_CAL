742ed56e2a2dc1fbc92bf50d96bd2ea1fb7e75c4
MechJM <matosjoaops@protonmail.com>
2020-03-06 12:25:21 +0000

    Removed unwanted folders

--- cal_fp01_CLion/cmake-build-debug/lib/googletest-master/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mechjm/Repositórios - Faculdade/2.º ano/2.º semestre/CAL/cal_fp01_CLion/lib/googletest-master
-# Build directory: /home/mechjm/Repositórios - Faculdade/2.º ano/2.º semestre/CAL/cal_fp01_CLion/cmake-build-debug/lib/googletest-master
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
