688d285642a00443c87e55c471ce27defd0dde8a
MechJM <matosjoaops@protonmail.com>
2020-04-17 17:43:14 +0100

    Class 6 exercises are done

--- cal_fp06_CLion/FichaJUNG.cpp
+++ cal_fp06_CLion/FichaJUNG.cpp
@@ -65,24 +65,94 @@ void exercicio2()
     gv->addEdge(11,9,11,EdgeType::DIRECTED);
     gv->addEdge(12,11,12,EdgeType::DIRECTED);
     gv->addEdge(13,11,13,EdgeType::DIRECTED);
-
+/*  //alinea c)
     gv->rearrange();
-
+    sleep(1);
     gv->removeNode(12);
     gv->removeNode(13);
     gv->addNode(14,250,550);
     gv->addNode(15,350,550);
-    sleep(1);
+
     gv->rearrange();
+*/
+    //alinea d) exemplo da professora
+    bool first = true;
+    while(1)
+    {
+        sleep(1);
+        if (first)
+        {
+            gv->removeNode(12);
+            gv->removeNode(13);
+            first=false;
+        }
+        else
+        {
+            gv->removeNode(20);
+            gv->removeNode(21);
+        }
+        gv->addNode(14,250,550);
+        gv->addNode(15,350,550);
+        gv->addEdge(14, 11, 14, EdgeType::UNDIRECTED);
+        gv->addEdge(15, 11, 15, EdgeType::UNDIRECTED);
+        gv->rearrange();
+        sleep(1);
+        gv->removeNode(14);
+        gv->removeNode(15);
+        gv->addNode(16,300,550);
+        gv->addNode(17,300,550);
+        gv->addEdge(16, 11, 16, EdgeType::UNDIRECTED);
+        gv->addEdge(17, 11, 17, EdgeType::UNDIRECTED);
+        gv->rearrange();
+        sleep(1);
+        gv->removeNode(16);
+        gv->removeNode(17);
+        gv->addNode(18,250,550);
+        gv->addNode(19,350,550);
+        gv->addEdge(18, 11, 18, EdgeType::UNDIRECTED);
+        gv->addEdge(19, 11, 19, EdgeType::UNDIRECTED);
+        gv->rearrange();
+        sleep(1);
+        gv->removeNode(18);
+        gv->removeNode(19);
+        gv->addNode(20,200,550);
+        gv->addNode(21,400,550);
+        gv->addEdge(20, 11, 20, EdgeType::UNDIRECTED);
+        gv->addEdge(21, 11, 21, EdgeType::UNDIRECTED);
+        gv->rearrange();
+    }
 }
 
 void exercicio3()
 {
-// TODO: Implement here exercise 3!
-// To read map files, use relative paths:
-// Vertices data: "../resources/mapa1/nos.txt"
-// Edges data: "../resources/mapa1/arestas.txt"
-// ...
+    GraphViewer* gv = new GraphViewer(600,600,false);
+    gv->createWindow(600,600);
+
+    ifstream nodeFile;
+    nodeFile.open("../resources/mapa1/nos.txt");
+
+    string line;
+    while (!nodeFile.eof())
+    {
+        getline(nodeFile,line);
+        int nodeID,nodeX,nodeY;
+        sscanf(line.c_str(), "%d;%d;%d", &nodeID, &nodeX, &nodeY);
+        gv->addNode(nodeID,nodeX,nodeY);
+    }
+    nodeFile.close();
+
+    ifstream edgeFile;
+    edgeFile.open("../resources/mapa1/arestas.txt");
+
+    while(!edgeFile.eof())
+    {
+        getline(edgeFile,line);
+        int edgeID,nodeOrigin,nodeDest;
+        sscanf(line.c_str(),"%d;%d;%d",&edgeID,&nodeOrigin,&nodeDest);
+        gv->addEdge(edgeID,nodeOrigin,nodeDest,EdgeType::DIRECTED);
+    }
+    edgeFile.close();
+    gv->rearrange();
 }
 
 int main() {
@@ -94,13 +164,13 @@ int main() {
     /*
       * Uncomment the line below to run Exercise 2
       */
-    exercicio2();
+    //exercicio2();
 
     /*
       * Uncomment the line below to run Exercise 3
       */
 	//
-	//exercicio3();
+	exercicio3();
 
 	getchar();
 	return 0;
